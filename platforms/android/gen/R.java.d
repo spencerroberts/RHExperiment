072bf51c5ae93a772cb77f0de8c7d073ef303a1c
spencerroberts <spencer.roberts@gmail.com>
2013-11-04 14:09:47 -0800

    adding first pass version

---
+++ platforms/android/gen/R.java.d
@@ -0,0 +1,9 @@
+C:\Users\Spencer\Documents\GitHub\RHExperiment\platforms\android\gen\org\rockharbor\experiment\R.java \
+ : C:\Users\Spencer\Documents\GitHub\RHExperiment\platforms\android\res\drawable\icon.png \
+C:\Users\Spencer\Documents\GitHub\RHExperiment\platforms\android\res\drawable-hdpi\icon.png \
+C:\Users\Spencer\Documents\GitHub\RHExperiment\platforms\android\res\drawable-ldpi\icon.png \
+C:\Users\Spencer\Documents\GitHub\RHExperiment\platforms\android\res\drawable-mdpi\icon.png \
+C:\Users\Spencer\Documents\GitHub\RHExperiment\platforms\android\res\drawable-xhdpi\icon.png \
+C:\Users\Spencer\Documents\GitHub\RHExperiment\platforms\android\res\values\strings.xml \
+C:\Users\Spencer\Documents\GitHub\RHExperiment\platforms\android\res\xml\config.xml \
+C:\Users\Spencer\Documents\GitHub\RHExperiment\platforms\android\bin\AndroidManifest.xml \
